512017fc18ebb23f677336c626919b815caf7235
John Figueiredo <jfigueir33@gmail.com>
2012-11-20 21:32:13 -0800

    second commit, file clean up

--- problems/project_euler_5.rb
+++ problems/project_euler_5.rb
@@ -3,10 +3,12 @@
 # Start Here:
 
 i = 0
-# basically infinant number count ;-)
 while true
    puts "The smallest number divisible by 1-20 is #{i}"
-   # multiply all prime numbers to get counter to perform this value instantly (1*2*3*5*7*11*13*17*19) = 9699690.  make sure to add 9699690 at end because it doesn't print last value. I still need to figure out how to print the last value with a 'break if' statement
+   # multiply all prime numbers to get counter to perform this value instantly (1*2*3*5*7*11*13*17*19) = 9699690. 
+   # Make sure to add 9699690 at end because it doesn't print last value. 
+   # I still need to figure out how to print the last value with a 'break if' statement
    i += 9699690
-   break if (i%11 == 0 && i%12 == 0 && i%13 == 0 && i%14 == 0 && i%15 == 0 && i%16 == 0 && i%17 == 0 && i%18 == 0 && i%19 == 0 && i%20 == 0)
+   break if (i%11 == 0 && i%12 == 0 && i%13 == 0 && i%14 == 0 && 
+    i%15 == 0 && i%16 == 0 && i%17 == 0 && i%18 == 0 && i%19 == 0 && i%20 == 0)
 end
