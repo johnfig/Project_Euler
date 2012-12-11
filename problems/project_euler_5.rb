5e36df0c4dbbaf5d6432befa313b226c512fd1d5
John Figueiredo <jfigueir33@gmail.com>
2012-12-10 21:20:40 -0800

    fizzbuzz test

--- problems/project_euler_5.rb
+++ problems/project_euler_5.rb
@@ -8,7 +8,7 @@ while true
    # multiply all prime numbers to get counter to perform this value instantly (1*2*3*5*7*11*13*17*19) = 9699690. 
    # Make sure to add 9699690 at end because it doesn't print last value. 
    # I still need to figure out how to print the last value with a 'break if' statement
-   i += 9699690
+   i += 210
    break if (i%11 == 0 && i%12 == 0 && i%13 == 0 && i%14 == 0 && 
     i%15 == 0 && i%16 == 0 && i%17 == 0 && i%18 == 0 && i%19 == 0 && i%20 == 0)
 end
