08ea13e61d35ac2e60c40e3b6dfb97c89b4861ae
John Figueiredo <john@johnfigueiredo.com>
2013-06-15 19:44:14 -0700

    Cleaned up files

--- project_euler_1_extra.rb
+++
@@ -1,8 +0,0 @@
-def PrintSum(max)
-  i = 0        
-  while i <= max
-    yield i
-    i = i += 5 #if (i%3 == 0 && i%5 == 0)
-  end
-end
-PrintSum(10000000) { |f| puts f, "" }
--- project_euler_6.rb
+++
@@ -1,6 +0,0 @@
-for num in 1..100
-	puts "#{num}" if (num%3 != 0 && num%5 != 0)
-	puts "fizz #{num}" if (num%3 == 0 && num%5 !=0)
-	puts "buzz #{num}" if (num%5 == 0 && num%3 !=0)
-	puts "fizzbuzz #{num}" if (num%3 == 0 && num%5 == 0)
-end
